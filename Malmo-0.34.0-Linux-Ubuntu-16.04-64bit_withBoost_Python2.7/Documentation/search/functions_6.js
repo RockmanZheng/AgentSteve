55a2dc5d3adc0db2c81246f2ff06bd63924f5dad
RockmanZheng <rockman-zheng@qq.com>
2018-06-06 10:03:48 +0800

    Remove Malmo, too big

--- Malmo-0.34.0-Linux-Ubuntu-16.04-64bit_withBoost_Python2.7/Documentation/search/functions_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['hasvalueondimension',['hasValueOnDimension',['../classmalmo_1_1_timestamped_reward.html#a4f01c8a1e69965e67ff4f53976b0c27a',1,'malmo::TimestampedReward']]]
-];
